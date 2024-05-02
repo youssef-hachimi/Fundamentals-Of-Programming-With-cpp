0704f9a3e27dcf78822a685e40fbe4a70d91dda1
youssef hachimi <83027476+youssef-hachimi@users.noreply.github.com>
2024-05-02 16:35:37 +0100

    Add files via upload

---
+++ 13-ConstantVar.cpp
@@ -0,0 +1,23 @@
+/*
+Constant Variable
+- Read Only Value
+- Can't Declare Without Value
+*/
+
+#include <iostream>
+using namespace std;
+#define DAYS 9
+
+int main()
+{
+    const int day = 8;
+    // int salary = 15000;
+    const int num = 100;
+    // num = 200;
+    cout << num;
+    // const int x;
+    cout << "\n"
+            << DAYS;
+    return 0;
+    return 0;
+}
