334d4fcaa71bcc9d098ebfc9dd4177abb667b3cd
youssef hachimi <83027476+youssef-hachimi@users.noreply.github.com>
2024-05-03 13:54:33 +0100

    Add files via upload

---
+++ CPP/9-15/cpp-assignments-9-to-15/assignment2.cpp
@@ -0,0 +1,32 @@
+// Type only one line at the beginning of the Code to show the results as in the outputs
+// Do not adjust the lines under suspension
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   // Write Your One Line Code Here
+
+//   // Do Not Edit Below
+//   a = 10, b = 20, c = 70;
+//   cout << "Sum Of All Number Is: " << a + b + c;
+//   return 0;
+// }
+// // Output
+
+// "Sum Of All Number Is: 100"
+
+// ===================================================================================
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Write Your One Line Code Here
+    int a,b,c;
+    // Do Not Edit Below
+    a = 10, b = 20, c = 70;
+    cout << "Sum Of All Number Is: " << a + b + c;
+    return 0;
+}
