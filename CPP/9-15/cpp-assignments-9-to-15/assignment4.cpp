4064bb11767d61d77f5570f5fb9e3ee894f1f9d9
youssef hachimi <83027476+youssef-hachimi@users.noreply.github.com>
2024-05-03 13:55:20 +0100

    Add files via upload

---
+++ CPP/9-15/cpp-assignments-9-to-15/assignment4.cpp
@@ -0,0 +1,35 @@
+// All you need is to write the value of the variable my_text in one line only to show the Output as in the example
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   // Write my_text Variable Content Here
+//   string my_text = "???";
+
+//   // Do Not Edit Below
+//   cout << my_text;
+//   return 0;
+// }
+
+// // Needed Output
+
+// ============
+// == Elzero ==
+// ============
+
+// =================================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Write my_text Variable Content Here
+    string my_text = "============\n== Elzero ==\n============\n";
+
+    // Do Not Edit Below
+    cout << my_text;
+    return 0;
+}
