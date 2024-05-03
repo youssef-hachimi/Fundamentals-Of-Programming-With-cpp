8a572a6804e1bf35b30ebbe8a4b88f15bd8112b7
youssef hachimi <83027476+youssef-hachimi@users.noreply.github.com>
2024-05-03 13:55:37 +0100

    Add files via upload

---
+++ CPP/9-15/cpp-assignments-9-to-15/assignment6.cpp
@@ -0,0 +1,31 @@
+// Type the line for printing the result to print the score 300
+// You should use all variables in the result print line
+// Use the right calculation to get you out of 300
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   // Do Not Edit Below
+//   int a = 210, b = -50, c = -40;
+
+//   // Write Your cout Code Here
+
+//   return 0;
+// }
+// ====================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Do Not Edit Below
+    int a = 210, b = -50, c = -40;
+
+    // Write Your cout Code Here
+    int result = a - (b+c);
+    cout << result;
+    return 0;
+}
