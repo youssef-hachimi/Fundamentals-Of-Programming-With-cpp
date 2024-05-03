5a038cae289e747e15765f61b56c16ded84a84ea
youssef hachimi <83027476+youssef-hachimi@users.noreply.github.com>
2024-05-03 13:57:14 +0100

    Add files via upload

---
+++ CPP/9-15/cpp-assignments-9-to-15/assignment8.cpp
@@ -0,0 +1,30 @@
+// Make the necessary adjustments to fix the Code to work and bring you the result which is number 19
+// int main()
+// {
+//   // Edit Here To Fix The Error
+//   const int current_year;
+//   const int birth_year;
+
+//   // Do Not Edit Here
+//   current_year = 2024;
+//   birth_year = 2005;
+//   int age_in_years = current_year - birth_year;
+//   cout << age_in_years;
+//   return 0;
+// }
+// ===============================================================================
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Edit Here To Fix The Error
+    int current_year;
+    int birth_year = 2005;
+
+    // Do Not Edit Here
+    current_year = 2024;
+    birth_year = 2005;
+    int age_in_years = current_year - birth_year;
+    cout << age_in_years;
+    return 0;
+}
