bf8243ef1fb16900f98a307d76a84c273a07a38c
youssef hachimi <83027476+youssef-hachimi@users.noreply.github.com>
2024-05-03 13:56:21 +0100

    Add files via upload

---
+++ CPP/9-15/cpp-assignments-9-to-15/assignment5.cpp
@@ -0,0 +1,35 @@
+// Add a software line that makes the program come out 300.
+// You must not add any final number in the written line
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   // Do Not Edit Below
+//   int a, b, c = 100;
+
+//   // Add Your Code Here
+
+//   // Do Not Edit Below
+//   cout << a + b + c; // 300
+//   return 0;
+// }
+
+// =========================================================================
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Do Not Edit Below
+    int a, b, c = 100;
+
+    // Add Your Code Here
+    a = 100;
+    b = 100;
+    c = 100;
+    // Do Not Edit Below
+    cout << a + b + c; // 300
+    return 0;
+}
