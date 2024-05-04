069ac2a2410905a60281987a2c62e6919d80620c
youssef hachimi <83027476+youssef-hachimi@users.noreply.github.com>
2024-05-04 14:45:51 +0100

    Add files via upload

---
+++ CPP/9-15/cpp-assignments-9-to-15/assignment11(challenge2).cpp
@@ -0,0 +1,35 @@
+// See the desired Output and note that it's all on one line
+// It is forbidden to edit any word in the line, not change the lines, not add distances
+// All you can do is add or remove Escaping Characters only
+// You can add any number you want until you reach the desired Output
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   cout << "Bello\nHello Elzeroo\n";
+//   cout << " Web Schoolll\n\n";
+//   cout << " I Love\n Programming\n";
+//   cout << " Too Much\t\t";
+//   cout << " Specially C++\n";
+//   return 0;
+// }
+
+// // Needed Output
+// "Hello Elzero Web School I Love Programming Too Much Specially C++"
+
+// ===========================================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\\Bello\rHello Elzeroo";
+    cout << " Web Schoolll";
+    cout << " I Love Programming";
+    cout << " Too Much";
+    cout << " Specially C++\n";
+    return 0;
+}
