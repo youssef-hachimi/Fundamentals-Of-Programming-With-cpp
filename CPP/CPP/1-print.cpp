d826d6b6427977e088b440b11ef6bea8be941671
youssef hachimi <83027476+youssef-hachimi@users.noreply.github.com>
2024-05-27 12:14:50 +0100

    Add files via upload

---
+++ CPP/CPP/1-print.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    int sum;
+    cout << "Type a number: ";
+    cin >>
+        x;
+    cout << "Type another number: ";
+    cin >>
+        y;
+    sum = x + y;
+    cout << "Sum is: " << sum;
+}
+// int x = 50;
+// int y = 50;
+// (x
+//      y)
+// {
+//     cout << "1";
+// }
+// (x
+//      y)
+// {
+//     cout << "2";
+// }
+// {
+//     cout << "3";
+// }
+// int x = 50;
+// int y = 50;
+// if (x ==
+//     y)
+// {
+//     cout << "Yes";
+// }
+// else
+// {
+//     cout << "No";
+// }
+
+// int x = 50;
+// int y = 50;
+// if (x ==
+//     y)
+// {
+//     cout << "1";
+// }
+// else if (x >
+//          y)
+// {
+//     cout << "2";
+// }
+// else
+// {
+//     cout << "3";
+// }
+
+// int day = 4;
+// switch (
+//     day)
+// {
+
+// case 1:
+//     cout << "Saturday";
+//     break;
+
+// case 2:
+//     cout << "Sunday";
+
+//     break;
+
+// default:
+//     cout << "Weekend";
+// }
+
+// for (int i = 0; i < 5;
+//      i++)
+// {
+//     cout << "Yes"
+//          << "\n";
+// }
+
+// for (int i = 0; i < 10; i++)
+// {
+//     if (i == 4)
+//     {
+
+//         continue;
+//     }
+//     cout << i << "\n";
+// }
+
+// void myFunction()
+// {
+//     cout << "I just got executed!";
+// }
+
+// int main()
+// {
+
+//     myFunction();
+
+//     myFunction();
+//     return 0;
+// }
+
+// const int myNum = 15;  // myNum will always be 15
+// myNum = 10;  // error: assignment of read-only variable 'myNum'
