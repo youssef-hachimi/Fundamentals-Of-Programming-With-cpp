6e2c77f5739f8c793eab4b07674bab8e1f68ee20
youssef hachimi <83027476+youssef-hachimi@users.noreply.github.com>
2024-05-28 23:19:23 +0100

    Add files via upload

---
+++ CPP/CPP/math-bolean.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+    using namespace std;
+
+int main()
+{
+    int x = 64;
+    cout << sqrt(x);
+    cout << round(2.6);
+    cout << log(3);
+    cout << max (5, 6);
+    cout << min (5, 6);
+    return 0;
+
+
+
+    int myAge = 25;
+int votingAge = 18;
+
+if (myAge >= votingAge) {
+    cout << "Old enough to vote!";
+} else {
+    cout << "Not old enough to vote.";
+}
+
+
+// Outputs: Old enough to vote!
+
+
+// if (condition1) {
+//   // block of code to be executed if condition1 is true
+// } else if (condition2) {
+//   // block of code to be executed if the condition1 is false and condition2 is true
+// } else {
+//   // block of code to be executed if the condition1 is false and condition2 is false
+// }
+
+
+int time = 22;
+if (time < 10) {
+    cout << "Good morning.";
+    } else if (time < 20) {
+    cout << "Good day.";
+    } else {
+    cout << "Good evening.";
+}
+// Outputs "Good evening."
+
+
+
+}
---
+++ CPP/CPP/oop.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+using std::string;
+
+// all Attributes in c++ class is private by default.
+int main(){
+
+}
